b1bc55c05c0ebd3fad84a5f8c3ad8f686d979f49
Gilson Alves <gilsonalvesdesouza@hotmail.com>
2021-10-21 16:49:36 -0300

    salvando URL na base de dados

--- src/controller/UrlController.ts
+++ src/controller/UrlController.ts
@@ -1,23 +1,35 @@
 import { NextFunction, Request, Response } from "express";
+import { StatusCodes } from "http-status-codes";
+import { URLModel } from "../model/URL";
 import shortid from "shortid";
 import { config } from "../config/Constants";
 
 class UrlController {
     public async shorten(req: Request, res: Response, next: NextFunction): Promise<void> {
         const { originURL } = req.body;
+
+        const url = await URLModel.findOne({ originURL });
+
+        if (url) {
+            res.json(url);
+            return
+        }
+
         const hash = shortid.generate();
         const shortURL = `${config.API_URL}/${hash}`;
+        const newURL = URLModel.create({ hash, shortURL, originURL });
         res.json({ originURL, hash, shortURL });
     }
 
     public async redirect(req: Request, res: Response, next: NextFunction): Promise<void> {
         const { hash } = req.params;
-        const url = {
-            "originURL": "https://www.youtube.com/",
-            "hash": "C94lEi-NS",
-            "shortURL": "http://localhost:5000/C94lEi-NS"
-          }
-          res.redirect(url.originURL);
+        const url = await URLModel.findOne({ hash });
+
+        if (url) {
+            res.redirect(url.originURL);
+        }
+
+        res.status(StatusCodes.NOT_FOUND).json({ error: "URL not found!" });
     }
 }
 
